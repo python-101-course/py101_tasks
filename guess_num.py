079cf1eb6b1442d2357f55a6f90bd8c5ad3cfcd7
Serghei Bordea <bordeaserghei@gmail.com>
2020-11-05 22:46:17 +0100

    Add functionality for guess_num.py

--- guess_num.py
+++ guess_num.py
@@ -13,6 +13,26 @@
 
 Тебе может понадобится модуль random, цикл while и ветвления
 """
+from random import randint
 
 if __name__ == '__main__':
-    pass
+    number = randint(0, 1_000_000)
+    print("Hello user. I picked a number from 0 to 1000000, try to guess it :)")
+    while True:
+        print("Write your number here ", end="")
+        user_guess_input = input()
+        try:
+            user_guess = int(user_guess_input)
+        except:
+            print("Please write a number")
+            continue
+        user_guess = int(user_guess_input)
+        if not (0 <= user_guess <= 1_000_000):
+            print("Please write a number between 0 and 1000000")
+        elif (user_guess < number):
+            print("Your number is smaller")
+        elif (user_guess > number):
+            print("Your number is bigger")
+        else:
+            print("You got it, nice job :)")
+            break
